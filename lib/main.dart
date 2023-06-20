050cc82b72556145f9a0440f4dbb6a42725ccb35
MarcSa <marc@marcvirtual.com>
2023-06-20 09:53:40 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.lightBlue[300],
+        appBar: AppBar(
+          title: Text('I Am Poor'),
+          backgroundColor: Colors.cyan[900],
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/coal.png'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
+
+
